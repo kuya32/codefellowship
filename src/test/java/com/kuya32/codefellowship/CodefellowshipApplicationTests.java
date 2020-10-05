ba3aee6957ddbb7334982effae666199571ccb5e
Marchael Acode <m.acode@outlook.com>
2020-10-05 16:20:16 -0700

    initial commit

---
+++ src/test/java/com/kuya32/codefellowship/CodefellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuya32.codefellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
