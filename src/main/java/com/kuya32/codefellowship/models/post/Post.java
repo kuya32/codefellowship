98f4a1dfba64e83443fd38c0e3ab080fb50676d3
Marchael Acode <m.acode@outlook.com>
2020-10-09 01:19:51 -0700

    Creating post model and setting getters/setters

--- src/main/java/com/kuya32/codefellowship/models/post/Post.java
+++ src/main/java/com/kuya32/codefellowship/models/post/Post.java
@@ -1,23 +1,27 @@
 package com.kuya32.codefellowship.models.post;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.kuya32.codefellowship.models.user.ApplicationUser;
+
+import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY);
     long id;
+
+    @ManyToOne
+    ApplicationUser applicationUser;
 
     String body;
     Timestamp createdAt;
 
     public Post() {};
 
-    public Post(String body, Timestamp createdAt) {
+    public Post(ApplicationUser applicationUser, String body, Timestamp createdAt) {
+        this.applicationUser = applicationUser;
         this.body = body;
         this.createdAt = createdAt;
     }
@@ -25,5 +29,29 @@ public class Post {
     // This makes a string of the post
     public String toString() {
         return String.format("%s\n Post made at %s", body, createdAt);
+    }
+
+    public ApplicationUser getApplicationUser() {
+        return applicationUser;
+    }
+
+    public void setApplicationUser(ApplicationUser applicationUser) {
+        this.applicationUser = applicationUser;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
     }
 }
